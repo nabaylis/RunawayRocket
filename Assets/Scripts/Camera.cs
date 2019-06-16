ba7cfb114fc9434c87aa198427592e260f0d7323
Noah Baylis <shogunwarrior101@gmail.com>
2019-06-16 17:14:05 -0400

    Please work

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;        //Public variable to store a reference to the player game object
+
+
+
+     void Update()
+    {
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(player.position.x, 0f, 0f),
+            Mathf.Clamp(player.position.y,-1f,1000000f),
+            transform.position.z
+        );
+    }
+}
+
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public float speed;
+    public float drift;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector2.up * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector2.right * drift * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector2.left * drift * Time.deltaTime);
+        }
+
+    }
+}
